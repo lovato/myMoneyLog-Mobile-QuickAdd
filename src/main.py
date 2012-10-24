cdd314c813f1556b7b0826c8e630ba90a224c224
Marco Lovato <maglovato@gmail.com>
2012-10-24 17:37:18 -0200

    PEP8 validation

--- src/main.py
+++ src/main.py
@@ -3,10 +3,11 @@ Created on 18/10/2012
 
 @author: marco.lovato
 '''
+#pylint: disable-msg=R0903,R0201
 import cherrypy
 import os.path
 import ConfigParser
-from mml_datafile_helper import Transactor, Transaction, MML_parser
+from mml_datafile_helper import Transactor, Transaction, Mmlparser
 from jinja2 import Environment, FileSystemLoader
 
 _CURDIR = os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -14,36 +15,39 @@ _CHERRYCONF = os.path.join(_CURDIR, 'cherrypy.conf')
 _APPCONF = os.path.join(_CURDIR, 'app.conf')
 ENV = Environment(loader=FileSystemLoader('templates'))
 
-#class RootOld(object):
-#    @cherrypy.expose
-#    def index(self):
-#        mmlp = MML_parser(os.path.join(get_mml_folder(), 'data.html'))
-#        temp = "<html><body><form action=/add>"
-#        temp = temp + 'date: <input name=date type=text><br>value: <
-#input name=value type=text><br>Desc: <input name=description type=text><br>\n'
-#        temp = temp + '<select name=at_account>\n'
-#        for item in mmlp.get_accounts():
-#            temp = temp+'<option>'+item+'</option>\n'
-#        temp = temp + '</select><br>\n'
-#        temp = temp + '<select name=tags>\n'
-#        for item in mmlp.get_categories():
-#            temp = temp+'<option>'+item+'</option>\n'
-#        temp = temp+'</select><br>\n<input type=submit></form></body></html>'
-#        return temp
-#        #return "<html><body><a href='/add'>oi</a></body></html>"
-
 
 class Root(object):
+    '''
+    Root
+    '''
     def index(self):
+        '''
+        index
+        '''
         tmpl = ENV.get_template('root.html')
-        mml = MML_parser(os.path.join(get_mml_folder(), 'data.html'))
-        return tmpl.render(accounts=mml.get_accounts(), categories=mml.get_categories())
+        mml = Mmlparser(os.path.join(get_mml_folder(), 'data.html'))
+        return tmpl.render(accounts=mml.get_accounts(),
+                           categories=mml.get_categories())
     index.exposed = True
 
 
 class AddEntry(object):
-    def index(self, date=None, account=None, description=None, value=None, tags=None, transaction_type=None, transaction_pending=None):
-        transaction = Transaction(date, value, description, tags, account, transaction_type, transaction_pending)
+    '''
+    AddEntry
+    '''
+    def index(self, date=None, account=None, description=None, value=None,
+              tags=None, transaction_type=None, transaction_pending=None):
+        '''
+        @param date:
+        @param account:
+        @param description:
+        @param value:
+        @param tags:
+        @param transaction_type:
+        @param transaction_pending:
+        '''
+        transaction = Transaction(date, value, description, tags, account,
+                                  transaction_type, transaction_pending)
         transactor = Transactor()
         transactor.set_mml_folder(get_mml_folder())
         transactor.append_transaction(transaction.get_formatted())
@@ -53,12 +57,22 @@ class AddEntry(object):
 
 
 class UserFeedback(object):
+    '''
+    UserFeedback
+    '''
     def index(self):
+        '''
+        index
+        '''
         tmpl = ENV.get_template('feedback.html')
         return tmpl.render(feedback_message = "Trasa&ccedil;&atilde;o adicionada")
     index.exposed = True
 
+
 def create_appconf_file():
+    '''
+    create_appconf_file
+    '''
     config = ConfigParser.RawConfigParser()
     config.add_section('global')
     config.set('global', 'mml_folder', r'C:\YourMMLFolder')
@@ -67,6 +81,9 @@ def create_appconf_file():
 
 
 def get_mml_folder():
+    '''
+    get_mml_folder
+    '''
     config = ConfigParser.RawConfigParser()
     config.read(_APPCONF)
     return config.get('global', 'mml_folder')
@@ -83,7 +100,6 @@ if __name__ == "__main__":
     cherrypy.tree.mount(Root(), "", _CHERRYAPPCONF)
     cherrypy.tree.mount(AddEntry(), "/add", _CHERRYAPPCONF)
     cherrypy.tree.mount(UserFeedback(), "/feedback", _CHERRYAPPCONF)
-    #cherrypy.tree.mount(Foo(), "/foo", _CHERRYAPPCONF)
 
     cherrypy.engine.start()
     cherrypy.engine.block()
--- src/mml_datafile_helper.py
+++ src/mml_datafile_helper.py
@@ -3,60 +3,85 @@ Created on 18/10/2012
 
 @author: marco.lovato
 '''
-
+#pylint: disable-msg=R0903,R0201
 import shutil
 import os.path
-#import ConfigParser
 
 
 class Transaction:
+    '''
+    class
+    '''
     date = None
     value = None
     description = None
     tags = None
     account = None
-    type = ""
+    signal = ""
     pending = ""
 
-    def __init__(self, date, value, description, tags, account, type, pending):
-#        print date
-#        print value
-#        print description
-#        print tags
-#        print account
-#        print type
-#        print pending
+    def __init__(self, date, value, description, tags,
+                 account, signal, pending):
+        '''
+        @param date:
+        @param value:
+        @param description:
+        @param tags:
+        @param account:
+        @param signal:
+        @param pending:
+        '''
         self.date = date
         self.value = value
         self.description = description
         self.tags = tags
         self.account = account
-        if type == "minus":
-            self.type = "-"
+        if signal == "minus":
+            self.signal = "-"
         if pending == "yes":
             self.pending = "?"
 
     def get_formatted(self):
-        return self.date + self.pending + "\t" + self.type + self.value + "\t" + self.description + "\t" + self.tags + "\t" + self.account
+        '''
+        get_formatted
+        '''
+        result = self.date + self.pending + "\t"
+        result = result + self.signal + self.value + "\t"
+        result = result + self.description + "\t" + self.tags + "\t"
+        result = result + self.account
+        return result
 
 
 class Transactor:
+    '''
+    class
+    '''
     last_transaction = None
     mml_folder = None
     datafile = None
     datafile_size = 0
 
+    def __init__(self):
+        pass
+
     def set_datafile(self, datafile):
-        import os.path
+        '''
+        @param datafile:
+        '''
         self.datafile = os.path.join(self.mml_folder, datafile)
         self._count_lines()
 
     def set_mml_folder(self, folder):
+        '''
+        @param folder:
+        '''
         self.mml_folder = folder
         self.set_datafile('data.html')
 
     def append_transaction(self, newline):
-        import os
+        '''
+        @param newline:
+        '''
         self.last_transaction = newline
 
         shutil.copy(self.datafile, self.datafile + '.tmp')
@@ -69,23 +94,16 @@ class Transactor:
             #shutil.move(self.datafile + '.new', self.datafile)
             infile.close()
             outfile.close()
-            try:
-                os.remove(self.datafile + '.tmp')
-            except Exception as e:
-                print e
-                return False
+            os.remove(self.datafile + '.tmp')
         self._count_lines()
         infile.close()
         outfile.close()
         return True
 
-#    def append_transaction(self, line):
-#        self.last_transaction = line
-#        with open(self.datafile, "a") as myfile:
-#            myfile.write('\n' + self.last_transaction)
-#        return True
-
     def get_last_transaction(self):
+        '''
+        get_last_transaction
+        '''
         with open(self.datafile, "r") as infile:
             for i, line in enumerate(infile):
                 if i == self.datafile_size - 2:
@@ -94,17 +112,28 @@ class Transactor:
         return self.last_transaction
 
     def remove_last_transaction(self):
+        '''
+        remove_last_transaction
+        '''
         return "removed"
 
     def _count_lines(self):
+        '''
+        _count_lines
+        '''
         if not os.path.exists(self.datafile):
             with open(self.datafile, 'w') as outfile:
                 outfile.write('<html>\n</html>')
                 outfile.close()
-        self.datafile_size = sum(1 for line in open(self.datafile, 'r'))
+        self.datafile_size = sum(1 for line in  # @UnusedVariable
+                                 open(self.datafile, 'r'))
         return self.datafile_size
 
-class MML_parser:
+
+class Mmlparser:
+    '''
+    class
+    '''
     _favorecidos = []
     _categories = []
     _accounts = []
@@ -112,46 +141,65 @@ class MML_parser:
     _datafile = None
 
     def _load_file_data(self):
+        '''
+        _load_file_data
+        '''
         with open(self._datafile) as infile:
             self._file_data = infile.readlines()
 
     def _parse_favorecidos(self):
+        '''
+        _parse_favorecidos
+        '''
         self._favorecidos = None
 
     def _parse_accounts_categories(self):
+        '''
+        _parse_favorecidos
+        '''
         self._categories = []
         self._accounts = []
         for line in self._file_data:
             if 'valor inicial\t' in line:
-                try:
-                    lixo, valid_data = line.split('valor inicial\t')
+                valid_data_array = line.split('valor inicial\t')
+                valid_data = valid_data_array[1]
+                valid_data = valid_data[:-1]
+                if valid_data.startswith("\t"):
+                    valid_data = valid_data[1:]
+                    self._accounts.append(valid_data)
+                if valid_data.endswith("\t"):
                     valid_data = valid_data[:-1]
-                    if valid_data.startswith("\t"):
-                        valid_data = valid_data[1:]
-                        self._accounts.append(valid_data)
-                    if valid_data.endswith("\t"):
-                        valid_data = valid_data[:-1]
-                        self._categories.append(valid_data)
-                except:
-                    pass
+                    self._categories.append(valid_data)
 
     def __init__(self, datafile):
+        '''
+        @param datafile:
+        '''
         self._datafile = datafile
         self._load_file_data()
         self._parse_accounts_categories()
         self._parse_favorecidos()
 
     def get_favorecidos(self):
+        '''
+        get_favorecidos
+        '''
         return sorted(self._favorecidos)
 
     def get_categories(self):
+        '''
+        get_categories
+        '''
         return sorted(self._categories)
 
     def get_accounts(self):
+        '''
+        get_accounts
+        '''
         return sorted(self._accounts)
 
 
 if __name__ == "__main__":
-    mmlp = MML_parser('U:\Marco\mymoneylog\data.html')
-    print mmlp.get_accounts()
-    print mmlp.get_categories()
+    MMLP = Mmlparser(r'U:\Marco\mymoneylog\data.html')
+    print MMLP.get_accounts()
+    print MMLP.get_categories()
